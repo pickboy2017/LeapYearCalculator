38dbfb415a18ec8bd527efdf67c5eb25f204d845
pickboy2017 <alpe6095@gmail.com>
2023-06-23 21:31:52 +0800

    my first commit for my LeapYearCalculator

---
+++ README.md
@@ -0,0 +1,1 @@
+# My Leap Year Calculator
---
+++ index.js
@@ -0,0 +1,24 @@
+function isLeap(isLeapYear) {
+  isLeapYear = prompt("Enter year: ");
+  divideBy4 = isLeapYear % 4 === 0; // check if year input is divisible by 4; true || false
+  divideBy100 = isLeapYear % 100 === 0; // check if year input is divisible by 100; true || false
+  divideBy400 = isLeapYear % 400 === 0; // check if year input is divisible by 400; true || false
+
+  if (divideBy4 === true) {
+    if (divideBy100 === true) {
+      if (divideBy400 === true) {
+        console.log(isLeapYear + " IS a Leap Year");
+      } else {
+        console.log(isLeapYear + " is NOT a Leap Year");
+      }
+    } else {
+      console.log(isLeapYear + " IS a Leap Year");
+    }
+  } else {
+    console.log(isLeapYear + " is NOT a Leap Year");
+  }
+
+  //return isLeap();
+}
+var result = isLeap();
+alert(result);
